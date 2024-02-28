8f0c6eb391b997d2aeb325c146f196afe2793550
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-29 00:11:13 +0530

    Added services, asyncronous data-subscribe

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -9,11 +11,32 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  constructor(private heroService: HeroService, private messageService : MessageService ){}
 
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  //Normal
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  //Observable --asynchronous
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  
+  
+
 }
